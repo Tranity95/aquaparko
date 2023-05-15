f94035d6f92c7619e5580bca7e84c13f155f14d4
Александр Глушенко <a228gdpro@gmail.com>
2023-05-15 15:15:43 +1000

    ALMOST DONE

--- aquaparko/AquaparkoContext.cs
+++ aquaparko/AquaparkoContext.cs
@@ -31,7 +31,7 @@ public partial class AquaparkoContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=192.168.200.13;user=student;password=student;database=aquaparko", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.3.38-mariadb"));
+        => optionsBuilder.UseMySql("server=localhost;user=root;password=Cartoxa_35000;database=aquaparko", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.3.38-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
