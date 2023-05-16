193d3c87f388db0b44e0f47ddd9a3bd88f051e89
FESQO <98529236+FESQO@users.noreply.github.com>
2023-05-16 15:03:52 +1000

    FINAL VERSION

---
+++ aquaparko/TicketsBGConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace aquaparko
+{
+    internal class TicketsBGConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            var date = (DateTime)value;
+
+            return date < DateTime.Now ? Brushes.Red : Brushes.Black;
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
